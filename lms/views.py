8533aa56d6dc18c773d91186030354874abb14e7
Rf <jonnyzver78@gmail.com>
2024-08-14 21:02:41 +1000

    init

--- lms/views.py
+++ lms/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from lms.models import Course, Lesson, SubscriptionCourse
 from lms.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer
-from lms.paginators import MaterialsPagination
+from lms.paginators import LmsPagination
 from users.permissions import IsModer, IsOwner
 
 
--- lms_project/settings.py
+++ lms_project/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from dotenv import load_dotenv
-
+from datetime import timedelta
 load_dotenv()
 import os
 
